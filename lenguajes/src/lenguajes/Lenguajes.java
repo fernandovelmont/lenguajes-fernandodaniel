923b351841737b364bd3a2604bb8ec513948beed
fernandovelmont <fernando_velmont@hotmail.com>
2017-01-16 19:36:55 -0600

    Update Lenguajes.java

--- lenguajes/src/lenguajes/Lenguajes.java
+++ lenguajes/src/lenguajes/Lenguajes.java
@@ -16,7 +16,7 @@ public class Lenguajes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo pero desde github");
     }
     
 }
