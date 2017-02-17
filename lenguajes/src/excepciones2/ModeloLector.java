d02914d1501f5aab79bd0b729231b3bb32a5aea5
Fernandodaniel <Fernandodaniel@Velazquez>
2017-02-16 19:39:28 -0600

    EXCEPCIONES

---
+++ lenguajes/src/excepciones2/ModeloLector.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Fernandodaniel
+ */
+public class ModeloLector {
+        public String leerArchivo(File file)throws FileNotFoundException, IOException{//trhows 
+            String informacion= "nada";
+           
+            FileInputStream fis=new FileInputStream(file);
+            StringBuilder builder=new StringBuilder(); //Sirve para construir strings que se van leyendo caracter por caracter 
+            int ch;
+            while((ch=fis.read())!=-1){ 
+                builder.append((char)ch); //append es agregar otro bloque para que se construya el archivo,casting para cambiar el tipo de dato
+            }
+            informacion=builder.toString();
+            return informacion;
+        }
+    
+}
